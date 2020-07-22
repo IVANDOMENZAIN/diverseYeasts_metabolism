6540b1cf9beb5b996299ba8e2706e10b68831c77
Ivan Domenzain <ivand@chalmers.se>
2020-07-22 18:31:00 +0200

    fix: explore production potential just for ecBmo at suboptimal gRates

--- code/explore_ecBmo.m
+++ code/explore_ecBmo.m
@@ -25,51 +25,55 @@ sol = solveLP(ecModel_batch,1);
 gRate_ec = sol.x(growthIdx);
 [modelRxns,modelIdxs] = getExchangeRxns(model);
 %Initialize variables for results
-excRxnIds    = [];
-modelRates   = [];
-ecModelRates = [];
-model_subRates   = [];
-ecModel_subRates = [];
-model_maxRates   = [];
-ecModel_maxRates = [];
+excRxnIds = [];
+rates_01  = [];
+rates_25  = [];
+rates_50  = [];
+rates_75  = [];
+rates_99  = [];
+
 for i=1:length(modelRxns)
     ecM_idx = find(strcmpi(ecModel_batch.rxns,modelRxns{i}));
     if ~isempty(ecM_idx)
         tempModel   = setParam(model,'obj',modelIdxs(i),1);
-        %Optimize for mximum production levels
-        tempModel   = setParam(tempModel,'ub',modelIdxs(i),1000);
+        %Optimize for maximum production levels
         tempecModel = setParam(ecModel_batch,'obj',ecM_idx,1);
-        solution    = solveLP(tempModel);
-        solution_ec = solveLP(tempecModel);
-        if ~isempty(solution.x) & ~isempty(solution_ec.x)
+        tempecModel = setParam(tempecModel,'lb',obj_ec,0.01*gRate_ec);
+        solution_01 = solveLP(tempecModel);
+        if ~isempty(solution_01.x)
             %Optimize for growth and production
-            tempModel   = setParam(tempModel,'lb',obj,0.5*gRate);
-            tempecModel = setParam(tempecModel,'lb',obj_ec,0.5*gRate_ec);
-            solution_sub    = solveLP(tempModel);
-            solution_sub_ec = solveLP(tempecModel);
-            if ~isempty(solution_sub.x) & ~isempty(solution_sub_ec.x)
-                %Release GUR and optimize
-                tempModel   = setParam(tempModel,'lb',obj,0.1);
-                tempecModel = setParam(tempecModel,'lb',obj_ec,0.1);
-                tempModel   = setParam(tempModel,'lb','r_1714',-1000);
-                tempecModel = setParam(tempecModel,'ub','r_1714_REV',1000);
-                solution_max   = solveLP(tempModel,1);
-                solution_max_ec = solveLP(tempecModel,1);
-                if ~isempty(solution_max.x) & ~isempty(solution_max_ec.x)
-                    disp(['#' num2str(i) ' ' model.rxnNames{modelIdxs(i)}])
-                    yield    = solution_max.x(modelIdxs(i))/abs(solution_max.x(GURidx));
-                    yield_EC = solution_max_ec.x(ecM_idx)/abs(solution_max_ec.x(ecGURidx));
-                    excRxnIds    = [excRxnIds; model.rxnNames(modelIdxs(i))];
-                    modelRates   = [modelRates;solution.x(modelIdxs(i))];
-                    ecModelRates = [ecModelRates;solution_ec.x(ecM_idx)];
-                    model_subRates   = [model_subRates;solution_sub.x(modelIdxs(i))];
-                    ecModel_subRates = [ecModel_subRates;solution_sub_ec.x(ecM_idx)];
-                    model_maxRates   = [model_maxRates;yield];
-                    ecModel_maxRates = [ecModel_maxRates;yield_EC];
+            tempecModel = setParam(ecModel_batch,'obj',ecM_idx,1);
+            tempecModel = setParam(tempecModel,'lb',obj_ec,0.25*gRate_ec);
+            solution_25 = solveLP(tempecModel);
+            if ~isempty(solution_25.x)
+                tempecModel = setParam(ecModel_batch,'obj',ecM_idx,1);
+                tempecModel = setParam(tempecModel,'lb',obj_ec,0.5*gRate_ec);
+                solution_50 = solveLP(tempecModel);
+                if ~isempty(solution_50.x)
+                    tempecModel = setParam(ecModel_batch,'obj',ecM_idx,1);
+                    tempecModel = setParam(tempecModel,'lb',obj_ec,0.75*gRate_ec);
+                    solution_75 = solveLP(tempecModel);
+                    if ~isempty(solution_75.x)
+                        tempecModel = setParam(ecModel_batch,'obj',ecM_idx,1);
+                        tempecModel = setParam(tempecModel,'lb',obj_ec,0.99*gRate_ec);
+                        solution_99 = solveLP(tempecModel);
+                        if ~isempty(solution_99.x)
+                            disp(model.rxnNames{modelIdxs(i)})
+                            excRxnIds = [excRxnIds;model.rxnNames(modelIdxs(i))];
+                            rates_01  = [rates_01;solution_01.x(ecM_idx)];
+                            rates_25  = [rates_25;solution_25.x(ecM_idx)];
+                            rates_50  = [rates_50;solution_50.x(ecM_idx)];
+                            rates_75  = [rates_75;solution_75.x(ecM_idx)];
+                            rates_99  = [rates_99;solution_99.x(ecM_idx)];
+                        end
+                    end
                 end
             end
         end
     end
 end
-results = table(excRxnIds,modelRates,ecModelRates,model_subRates,ecModel_subRates,model_maxRates,ecModel_maxRates);     
+results = table(excRxnIds,rates_01,rates_25,rates_50,rates_75,rates_99); 
+sumas = sum(table2array(results(:,2:end)),2);
+results = results(sumas>0,:);
+writetable(results,'../results/Bmo_production_glc_potential.txt','delimiter','\t','QuoteStrings',false);
     
