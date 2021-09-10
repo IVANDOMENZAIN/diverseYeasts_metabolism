464eaaf198664927fab0f8288d5e8d5e58acd6a0
abrilvalras <75130047+abrilvalras@users.noreply.github.com>
2021-09-10 14:57:07 +0200

    chore: come to the realization that I need to block lelor genes first xd

--- code/explore_galactitol.m
+++ code/explore_galactitol.m
@@ -1,3 +1,29 @@
 %explore galactitol production in C. intermedia model
 load('cintGEM_oxido_AVR.mat')
+%See how exchange fluxes look
+sol = solveLP(model,1);
+printFluxes(model,sol.x)
+%Model is already growing on lactose, growth is 0.20682
+%We need to block the leloir genes first to see galactitol production!!
 
+
+
+
+
+
+%Let's find the galactitol exchange reaction
+gtoh = find(contains(model.rxns,'ald_red_NADH'));
+printModel(model,gtoh) %not exchange, but formation! we're getting close
+
+
+
+
+
+
+gtoh = find(contains(model.rxnNames,'ol exchange'));
+model.rxnNames(gtoh) 
+%apparently it doesn't exist! S: Let's add it:
+newRxns = {'D-galactose[c] + NADPH[c] => galactitol[c] + NADP(+)[c]'... 
+           'L-xylo-3-hexulose[c] + NADPH[c] + H+[c] <=> L-sorbose[c] + NADP(+)[c]'};...
+           %'D-Fructose[c] + ATP[c] <=> D-Fructose-6-Phosphate[c] + ADP[c]'};
+rxnsToAdd.equations = newRxns;
