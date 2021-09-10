2cc2dceae576c6cc39e359da5985b40ba91a4bad
abrilvalras <75130047+abrilvalras@users.noreply.github.com>
2021-09-10 16:17:48 +0200

    chore: try to add galactitol exchange reaction

--- code/explore_galactitol.m
+++ code/explore_galactitol.m
@@ -7,23 +7,32 @@ printFluxes(model,sol.x)
 %We need to block the leloir genes first to see galactitol production!!
 
 
-
-
-
-
+gtoh = find(contains(model.metNames,'galactitol'));
+model.metNames(gtoh)
+model.metComps(gtoh) %We already have gtoh(c) and gtoh(e)
+sol.x(gtoh) %No flux in any of them
 %Let's find the galactitol exchange reaction
-gtoh = find(contains(model.rxns,'ald_red_NADH'));
-printModel(model,gtoh) %not exchange, but formation! we're getting close
+gtoh_x = find(contains(model.rxnNames,'tol exchange'));
+model.rxnNames(gtoh_x)
+printModel(model,gtoh_x) %D-glucitol[e] =>  [0 1000]
+
+lac_x = find(contains(model.rxnNames,'lactose exchange'));
+model.rxnNames(lac_x) %lactose[e] <=>  [-1 1000]
+printModel(model,lac_x)
 
 
-
-
-
-
-gtoh = find(contains(model.rxnNames,'ol exchange'));
-model.rxnNames(gtoh) 
-%apparently it doesn't exist! S: Let's add it:
-newRxns = {'D-galactose[c] + NADPH[c] => galactitol[c] + NADP(+)[c]'... 
-           'L-xylo-3-hexulose[c] + NADPH[c] + H+[c] <=> L-sorbose[c] + NADP(+)[c]'};...
-           %'D-Fructose[c] + ATP[c] <=> D-Fructose-6-Phosphate[c] + ADP[c]'};
+%it looks like it doesn't exist in the model! Let's add it
+newRxns = {'galactitol[c] <=> galactitol[e]'};
 rxnsToAdd.equations = newRxns;
+rxnsToAdd.rxnNames = {'galactitol exchange'};
+rxnsToAdd.rxns     = {'gtoh_ex'};
+%Define objective and bounds
+rxnsToAdd.c  = 0;
+rxnsToAdd.lb = 1000;
+rxnsToAdd.ub = 1000;
+% %genes to add
+rxnsToAdd.grRules = {'xyl1_2'};
+% Introduce changes to the model
+model_oxido = addRxns(model_oxido,rxnsToAdd,3);
+
+
