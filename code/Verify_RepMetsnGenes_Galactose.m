22616d99340faaad5cc22b286ca880486c4d279d
Kamesh <67782953+kdeperi@users.noreply.github.com>
2024-03-08 18:26:44 +0100

    Feat: Exploration of additional L-glucitol genes

--- code/Verify_RepMetsnGenes_Galactose.m
+++ code/Verify_RepMetsnGenes_Galactose.m
@@ -54,7 +54,7 @@ GalGenes3 = model.grRules(GalRxns3);
 disp(GalGenes3)
 
 
-otherGenes = {'Seq_4642','Seq_1183','Seq_2189','Seq_1160','Seq_4006','Seq_758','Seq_5357','Seq_2923','Seq_5493','Seq_4936','Seq_4728'};
+otherGenes = {'Seq_4642','Seq_1183','Seq_2189','Seq_1160','Seq_4006','Seq_758','Seq_5493','Seq_4936','Seq_4728'};
 
 %Here we go gene by gene finding associated reacitons and print the
 %formulas
@@ -62,6 +62,8 @@ for i=1:length(otherGenes)
     genePos = find(strcmp(model.genes,otherGenes{i}));
     geneAssRxns = find(model.rxnGeneMat(:,genePos));
     disp(otherGenes{i})
+    (model.rxns{geneAssRxns})
+    (model.rxnNames{geneAssRxns})
     constructEquations(model,geneAssRxns,true)
     disp(' ')
 end
@@ -69,12 +71,61 @@ end
 %we found that Seq_2189 is annotated as XYL2 in the genome and in the RNA_2_model_glu_vs_gal file is 5 times log2 fold upregulated and 2.5 times in lactose. the p value is significant.
 %found also that Glucitol is present once again in the list of reporter
 %metabolites. Checked the associated genes and found two new ones (Seq_5357
-%= YALI0B16192G which is a putative reductase, is 3 fold upregulation with low p-value), (Seq_2923 = SOU2 which is a sorbose dehydrogenase -1.5 fold change and
+%= YALI0B16192G which is a putative reductase, is 3 fold upregulation with low p-value), (Seq_2923 = SOU2 which is a sorbose dehydrogenase -1.9 fold change and
 %low p-value), (Seq_2552 = lad highly 6 fold upreg in galactose only)
 %also found galactose in the repmets_glu_gal but no surprises. another one
 %is Seq_2272 = YMR226c which I have deleted, but its not really significant in lactose, but is in galactose.
 
 
 
+otherGenes = {'Seq_5357','Seq_2923'};
 
+%Here we go gene by gene finding associated reacitons and print the
+%formulas
+for i=1:length(otherGenes)
+    genePos = find(strcmp(model.genes,otherGenes{i}));
+    geneAssRxns = find(model.rxnGeneMat(:,genePos));
+    disp(otherGenes{i})
+    (model.rxns{geneAssRxns})
+    (model.rxnNames{geneAssRxns})
+    constructEquations(model,geneAssRxns,true)
+    disp(' ')
+end
 
+%gene Seq_2923 is associated to reaction r_5174 which is D-glucitol:DP
+%oxidoreductase. It seems that the Seq_2923 is NADH dependent reversible
+%reaction is down regulated whereas Seq_2189 encoding r_0691 is upregulated
+%and is not reversible. 
+% we find redundancy in the glucitol dehydrogenase step among these genes.
+% But they also have other options but also some oxidoreductase reactions
+% involving erythritol, in particular the Seq_2923 encodes for MNXR121148.
+% This sequence is downregulated, does that mean its competing with the
+% oxidoreductive pathway?
+
+%identify relevant rxns and mets associated to erythritol[c] in the model 
+posEry = find(strcmp(model.metNames,'erythritol'));
+%Verify erythritol presence 
+disp(model.metNames(posEry))
+disp(model.metComps(posEry))
+disp(model.compNames)
+
+posEry = posEry(1);
+EryRxns = find(model.S(posEry,:));
+constructEquations(model,EryRxns,true)
+EryRxns = EryRxns(1);
+EryGenes = model.grRules(EryRxns);
+disp(EryGenes)
+
+%now checking erythrulose
+posEry = find(strcmp(model.metNames,'D-erythrulose 4-phosphate'));
+%Verify erythritol presence 
+disp(model.metNames(posEry))
+disp(model.metComps(posEry))
+disp(model.compNames)
+
+posEry = posEry(1);
+EryRxns = find(model.S(posEry,:));
+constructEquations(model,EryRxns,true)
+EryRxns = EryRxns(1);
+EryGenes = model.grRules(EryRxns);
+disp(EryGenes)
