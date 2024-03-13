dff0d1a5ca90a888fceccfa2bd417b78c6f5df2c
Kamesh <67782953+kdeperi@users.noreply.github.com>
2024-03-13 18:19:19 +0100

    feat: add fitGAm function
    Adapted to non-GECKO model

---
+++ code/fitGAM.m
@@ -0,0 +1,69 @@
+function GAM = fitGAM(model)
+% fitGAM
+%
+% GAM = fitGAM(model,verbose)
+% Returns a fitted GAM for the metabolic model (non-GECKO).
+%
+% Usage: GAM = fitGAM(model)
+%
+
+%Load chemostat data:
+fid = fopen('../data/chemostatData_glucose.txt','r');
+exp_data = textscan(fid,'%f32 %f32 %f32 %f32','Delimiter','\t','HeaderLines',1);
+exp_data = [exp_data{1} exp_data{2} exp_data{3} exp_data{4}];
+fclose(fid);
+%GAMs to span:
+disp('Estimating GAM:')
+GAM = 20:5:100;
+%1st iteration:
+GAM = iteration(model,GAM,exp_data);
+%2nd iteration:
+GAM = iteration(model,GAM-10:1:GAM+10,exp_data);
+%3rd iteration:
+[GAM,error] = iteration(model,GAM-1:0.1:GAM+1,exp_data);
+%If verbose output is not required, then the only displayed value is the optimal one
+disp(['Fitted GAM = ' num2str(GAM) ' -> Error = ' num2str(error)])
+
+%Plot fit:
+mod_data = simulateChemostat(model,exp_data,GAM);
+figure
+hold on
+cols = [0,1,0;0,0,1;1,0,0];
+b    = zeros(1,length(exp_data(1,:))-1);
+for i = 1:length(exp_data(1,:))-1
+    b(i) = plot(mod_data(:,1),mod_data(:,i+1),'Color',cols(i,:),'LineWidth',2);
+    plot(exp_data(:,1),exp_data(:,i+1),'o','Color',cols(i,:),'MarkerFaceColor',cols(i,:))
+end
+title('GAM fitting for growth on glucose minimal media')
+xlabel('Dilution rate [1/h]')
+ylabel('Exchange fluxes [mmol/gDWh]')
+legend(b,'Glucose consumption','O2 consumption','CO2 production','Location','northwest')
+hold off
+disp(' ')
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [GAM,error] = iteration(model,GAM,exp_data)
+
+fitting = ones(size(GAM))*1000;
+
+for i = 1:length(GAM)
+    %Simulate model and calculate fitting:
+    mod_data   = simulateChemostat(model,exp_data,GAM(i));
+    R          = (mod_data - exp_data)./exp_data;
+    fitting(i) = sqrt(sum(sum(R.^2)));
+    disp(['GAM = ' num2str(GAM(i)) ' -> Error = ' num2str(fitting(i))])
+end
+
+%Choose best:
+[error,best] = min(fitting);
+
+if best == 1 || best == length(GAM)
+    error('GAM found is sub-optimal: please expand GAM search bounds.')
+else
+    GAM   = GAM(best);
+end
+
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
